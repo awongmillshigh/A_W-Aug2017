a5eace700407c4b29c8e595a435088002e049acb
awongmillshigh <anthonywong888@yahoo.com>
2017-08-24 09:30:53 -0700

    My Third Time ;)

---
+++ Hello_World/src/Hello_World.java
@@ -0,0 +1,11 @@
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World!");
+		System.out.println("Hello 1");
+		System.out.println("Hello 2");
+
+	}
+
+}
