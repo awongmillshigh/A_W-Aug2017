cdbe61f19692fff73a91205e4cb6870a7f7b77e4
awongmillshigh <31107151+awongmillshigh@users.noreply.github.com>
2017-09-10 17:42:38 -0700

    Update DoMath.java

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -9,7 +9,10 @@ public class DoMath {
 		System.out.println(Calculate.average(4,6));
 		System.out.println(Calculate.average(3,4,5));
 		System.out.println(Calculate.toDegrees(90));
-		
-		
+		System.out.println(Calculate.toRadians(45));
+		System.out.println(Calculate.discriminant(2,4,6));
+		System.out.println(Calculate.toImproperFrac(3,1,2));
+		System.out.println(Calculate.toMixNum(7,2));
+		System.out.println(Calculate.foil(2,3,6,-7,"n"));
 	}
 }
