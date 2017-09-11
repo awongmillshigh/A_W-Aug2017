ae0d474839bfe25b3e4c043c6a94b68bed5c039c
awongmillshigh <31107151+awongmillshigh@users.noreply.github.com>
2017-09-10 17:42:46 -0700

    Update Calculate.java

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -37,7 +37,33 @@ public class Calculate {
 	
 	// convert to radians
 	public static double toRadians(double operand) {
-		double toRadians = (operand*)
+		double toRadians = operand*3.14159/180;
+		retun toRadians;
 	}
+	
+	// calculates using discriminant
+	public static double discriminant(double oeprand1, double operand2, doubl operand3){
+		double discriminant = operand2*operand2-4operand1*operand3;
+		return discriminant;
+	}
+	
+	//convertes mixed number into an improper fraction
+	public static double toImproperFrac(int operand1, int operand2, int operand3){
+		int toImproperFrac = operand1*operand3+operand2;
+		return toImproperFrac;
+	}
+	
+	//converts an imporproper fraction into a mixed number
+	public static double toMixedNum(int operand1, int operand2){
+		int toMixedNum = operand1/operand2;
+		double toWholeNum = 
+		return toMixedNuml	
+	}
+	
+	//converts a binomial mulitiplication form to a quadratic equaion form
+	public static double foil(int operand1, int operand2, int operand3, int operand4){
+		int foil = operand1*operand3+operand1*operand4+operand2*operand3+operand2*operand4;
+		return foil;
+		
 	
 }
