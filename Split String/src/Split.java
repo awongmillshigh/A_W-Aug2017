8eacbb08f206d70ae581a04561b5576968785072
awongmillshigh <31107151+awongmillshigh@users.noreply.github.com>
2017-10-30 09:23:23 -0700

    Update Split.java

--- Split String/src/Split.java
+++ Split String/src/Split.java
@@ -2,8 +2,9 @@ import java.util.*;
 public class Split {
 
 	public static void main(String[] args) {
-		String sandwhich = "applespineapplesbreadlettustomatobaconmayohambreadcheese";
-		System.out.println(sandwhich.split("bread"));
+		String[] sandwhich = "applespineapplesbreadlettustomatobaconmayohambreadcheese".split("");
+		String middle = "";
+		System.out.println(Array.toString.("bread").split);
 				
 		String sandwhich2 = "apples pineapples bread lettus tomato bacon mayo ham bread cheese";
 		System.out.println(sandwhich2.split("bread"));
