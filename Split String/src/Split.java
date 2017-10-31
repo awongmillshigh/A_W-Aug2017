6ab59fc0f15ecacc9d8480419ce1121f7bd19584
awongmillshigh <31107151+awongmillshigh@users.noreply.github.com>
2017-10-31 02:21:16 -0700

    Update Split.java
    new

--- Split String/src/Split.java
+++ Split String/src/Split.java
@@ -2,12 +2,39 @@ import java.util.*;
 public class Split {
 
 	public static void main(String[] args) {
-		String[] sandwhich = "applespineapplesbreadlettustomatobaconmayohambreadcheese";
-		String middle = "";
-		System.out.println(Array.toString.("bread").split);
-				
-		String sandwhich2 = "apples pineapples bread lettus tomato bacon mayo ham bread cheese";
-		System.out.println(sandwhich2.split("bread"));
+		String sandwich = "applespineapplesbreadlettustomatobaconmayohambreadcheese";
+		String[] filling = sandwich.split("bread");
+		for (String breakSandwich : filling) {
+		    System.out.println(breakSandwich);
+		}
 	}
-
+		
+	String sandwich2 = "apples pineapples bread lettus tomato bacon mayo ham bread cheese";
+	System.out.println(middleSandwich(sandwich2));
+		
+	public static String middleSandwich(String sandwich2) {
+		String[] toppings = new String[sandwich2.split(" ").length];
+		int numBread = 0;
+		for(int i = 0; i < toppings.length; i++) {
+			toppings[i] = sandwich2.split(" ")[i];
+			if(toppings[i].equals("bread")) {
+				numBread++;
+			}
+		}
+			
+		int[] breadSlice = new int[numBread];
+		int counter = 0;
+		for (int i = 0; i < toppings.length; i++) {
+			if (toppings[i].equals("bread")) {
+				breadSlice[counter] = i;
+				counter++;
+			}
+		}	
+		String middle = "";
+		for (int i = breadSlice[0] + 1; i < breadSlice[breadSlice.length - 1]; i++) {
+			middle += toppings[i] + " ";
+		}	
+		return middle;
+		}
+	
 }
